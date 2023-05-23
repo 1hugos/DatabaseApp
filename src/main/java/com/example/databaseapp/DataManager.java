377823055e88d4e7e93e432e74011ba22a66aee4
Marcin <marcinstankowsky@gmail.com>
2023-05-23 22:03:13 +0200

    Added spinner and checkbox functionalities

--- src/main/java/com/example/databaseapp/DataManager.java
+++ src/main/java/com/example/databaseapp/DataManager.java
@@ -11,88 +11,134 @@ public class DataManager {
     public static final String TABLE_ROW_ID = "_id";
     public static final String TABLE_ROW_NAME = "name";
     public static final String TABLE_ROW_LASTNAME = "lastname";
+    public static final String TABLE_ROW_PESEL = "pesel";
+    public static final String TABLE_ROW_AGE = "age";
+    public static final String TABLE_ROW_GENDER = "gender";
     public static final String DB_NAME = "address_book_db";
-    public static final int DB_VERSRION = 1;
+    public static final int DB_VERSION = 3;
     private static final String TABLE_N_AND_L = "names_and_lastnames";
 
 
 
-    private class CustomSQLiteOpenHelper extends SQLiteOpenHelper{
+    private static class CustomSQLiteOpenHelper extends SQLiteOpenHelper{
         public CustomSQLiteOpenHelper(Context context){
-            super(context, DB_NAME, null, DB_VERSRION);
+            super(context, DB_NAME, null, DB_VERSION);
         }
 
         public void onCreate(SQLiteDatabase db){
-            String newTableQueryString = "create table "
+            String newTableQueryString = "CREATE TABLE "
                     + TABLE_N_AND_L + " ("
                     + TABLE_ROW_ID
-                    + " integer primary key autoincrement not null,"
+                    + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "
                     + TABLE_ROW_NAME
-                    + " text not null,"
+                    + " TEXT NOT NULL, "
                     + TABLE_ROW_LASTNAME
-                    + " text not null);";
+                    + " TEXT NOT NULL, "
+                    + TABLE_ROW_AGE
+                    + " TEXT NOT NULL, "
+                    + TABLE_ROW_GENDER
+                    + " TEXT NOT NULL, "
+                    + TABLE_ROW_PESEL
+                    + " TEXT NOT NULL);";
 
             db.execSQL(newTableQueryString);
         }
-
-        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion){
-
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + TABLE_N_AND_L);
+            onCreate(db);
         }
     }
-
 
     public DataManager(Context context){
         CustomSQLiteOpenHelper helper = new CustomSQLiteOpenHelper(context);
         db = helper.getWritableDatabase();
     }
 
-    public void insert (String name, String lastName){
+    public void insert (String name, String lastName, String age, String gender, String ssn){
         String query = "INSERT INTO "
                 + TABLE_N_AND_L
                 + " ("
                 + TABLE_ROW_NAME
                 + ", "
                 + TABLE_ROW_LASTNAME
+                + ", "
+                + TABLE_ROW_AGE
+                + ", "
+                + TABLE_ROW_GENDER
+                + ", "
+                + TABLE_ROW_PESEL
                 + ") "
                 + "VALUES ("
                 + "'"
                 + name
-                + "'"
-                + ", "
+                + "', "
                 + "'"
                 + lastName
+                + "', "
+                + "'"
+                + age
+                + "', "
+                + "'"
+                + gender
+                + "', "
+                + "'"
+                + ssn
                 + "');";
 
         Log.i("insert() = ", query);
         db.execSQL(query);
-
     }
 
     public void delete(String name){
         String query = "DELETE FROM "
                 + TABLE_N_AND_L
-                + " WHEERE "
+                + " WHERE "
                 + TABLE_ROW_NAME
                 + " = '"
                 + name
                 + "';";
 
         Log.i("delete() = ", query);
+        db.execSQL(query);
     }
 
     public Cursor selectAll(){
-        Cursor c = db.rawQuery("SELECT * from " + TABLE_N_AND_L, null);
+        String spinnerCategory = MainActivity.categorySpinner.getSelectedItem().toString();
+
+        String query = "";
+
+        if(spinnerCategory.equals("Male")){
+            query = "SELECT * FROM "
+                    + TABLE_N_AND_L
+                    + " WHERE "
+                    + TABLE_ROW_GENDER
+                    + " = '"
+                    + "Male"
+                    + "';";
+        } else if(spinnerCategory.equals("Female")){
+            query = "SELECT * FROM "
+                    + TABLE_N_AND_L
+                    + " WHERE "
+                    + TABLE_ROW_GENDER
+                    + " = '"
+                    + "Female"
+                    + "';";
+        } else if(spinnerCategory.equals("Adult")){
+            query = "SELECT * FROM "
+                    + TABLE_N_AND_L
+                    + " WHERE "
+                    + TABLE_ROW_AGE
+                    + " >= 18;";
+        } else if(spinnerCategory.equals("All results")){
+            query = "SELECT * from " + TABLE_N_AND_L;
+        }
+
+        Cursor c = db.rawQuery(query, null);
         return c;
     }
 
     public Cursor searchName(String name){
-        String query = "SELECT "
-                + TABLE_ROW_ID
-                + ", "
-                + TABLE_ROW_NAME
-                + ", "
-                + TABLE_ROW_LASTNAME
-                + " FROM"
+        String query = "SELECT * FROM "
                 + TABLE_N_AND_L
                 + " WHERE "
                 + TABLE_ROW_NAME
--- src/main/java/com/example/databaseapp/MainActivity.java
+++ src/main/java/com/example/databaseapp/MainActivity.java
@@ -5,11 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.util.Log;
 import android.database.Cursor;
 import android.widget.Spinner;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.math.BigInteger;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -19,12 +27,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public Button searchBtn;
     public EditText nameEditTxt;
     public EditText lastNameEditTxt;
-    public EditText ssnEditTxt;
+    public EditText peselEditTxt;
     public EditText ageEditTxt;
-    public Spinner genderSpinner;
     public EditText deleteEditTxt;
     public EditText searchEditTxt;
-
+    public static Spinner categorySpinner;
+    public Spinner genderSpinner;
+    public TableLayout dataTable;
     DataManager dm;
 
     @SuppressLint("MissingInflatedId")
@@ -39,11 +48,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         searchBtn = (Button)findViewById(R.id.searchBtn);
         nameEditTxt = (EditText)findViewById(R.id.nameEditTxt);
         lastNameEditTxt = (EditText)findViewById(R.id.lastnameEditTxt);
-        ssnEditTxt= (EditText)findViewById(R.id.ssnEditTxt);
+        peselEditTxt= (EditText)findViewById(R.id.peselEditTxt);
         ageEditTxt= (EditText)findViewById(R.id.ageEditTxt);
-        genderSpinner=(Spinner) findViewById(R.id.genderSpinner);
         deleteEditTxt = (EditText)findViewById(R.id.deleteEditTxt);
         searchEditTxt = (EditText)findViewById(R.id.searchEditTxt);
+        dataTable = (TableLayout)findViewById(R.id.dataTable);
+        categorySpinner = (Spinner)findViewById(R.id.categorySpinner);
+        genderSpinner = (Spinner)findViewById(R.id.genderSpinner);
 
         dm = new DataManager(this);
 
@@ -51,33 +62,269 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         deleteBtn.setOnClickListener(this);
         insertBtn.setOnClickListener(this);
         searchBtn.setOnClickListener(this);
+
+        // Tworzenie adaptera dla spinnera z czterema opcjami
+        ArrayAdapter<String> categoryAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,
+                new String[]{"All results", "Male", "Female", "Adult"});
+        categoryAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        categorySpinner.setAdapter(categoryAdapter);
+
+        // Tworzenie adaptera dla spinnera z dwoma opcjami
+        ArrayAdapter<String> genderAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item,
+                new String[]{"Male", "Female"});
+        genderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        genderSpinner.setAdapter(genderAdapter);
     }
 
-    public void showData(Cursor c){
-        while(c.moveToNext()){
-            Log.i(c.getString(1),c.getString(2));
+    public void showData(Cursor cursor){
+        if (cursor.moveToFirst()) {
+            // Iteruj przez wszystkie wiersze kursora
+            do {
+                // Pobierz wartości kolumn dla bieżącego wiersza
+                @SuppressLint("Range") String name = cursor.getString(cursor.getColumnIndex(DataManager.TABLE_ROW_NAME));
+                @SuppressLint("Range") String lastname = cursor.getString(cursor.getColumnIndex(DataManager.TABLE_ROW_LASTNAME));
+                @SuppressLint("Range") String age = cursor.getString(cursor.getColumnIndex(DataManager.TABLE_ROW_AGE));
+                @SuppressLint("Range") String gender = cursor.getString(cursor.getColumnIndex(DataManager.TABLE_ROW_GENDER));
+                @SuppressLint("Range") String pesel = cursor.getString(cursor.getColumnIndex(DataManager.TABLE_ROW_PESEL));
+
+                System.out.println("Name: " + name);
+                System.out.println("Last Name: " + lastname);
+                System.out.println("Age: " + age);
+                System.out.println("Gender: " + gender);
+                System.out.println("Pesel: " + pesel);
+
+            } while (cursor.moveToNext());
         }
+
+        if (cursor.moveToFirst()) {
+            // Tworzenie wiersza nagłówka
+            TableRow headerRow = new TableRow(this);
+            headerRow.setLayoutParams(new TableRow.LayoutParams(
+                    TableRow.LayoutParams.MATCH_PARENT,
+                    TableRow.LayoutParams.WRAP_CONTENT));
+
+            // Dodawanie nagłówków kolumn
+            String[] columnNames = cursor.getColumnNames();
+            for (int i = 0; i < columnNames.length; i++) {
+                TextView headerTextView = new TextView(this);
+                headerTextView.setText(columnNames[i]);
+
+                TableRow.LayoutParams layoutParams;
+                if (i == 0) {
+                    // Ustawienie layout_weight dla komórki ID na 2
+                    layoutParams = new TableRow.LayoutParams(
+                            0,
+                            TableRow.LayoutParams.WRAP_CONTENT,
+                            0.3f);
+                }else if( i == 1) {
+                    // Ustawienie layout_weight dla pozostałych komórek na 1
+                    layoutParams = new TableRow.LayoutParams(
+                            0,
+                            TableRow.LayoutParams.WRAP_CONTENT,
+                            0.8f);
+                }else if( i == 3) {
+                    // Ustawienie layout_weight dla pozostałych komórek na 1
+                    layoutParams = new TableRow.LayoutParams(
+                            0,
+                            TableRow.LayoutParams.WRAP_CONTENT,
+                            0.4f);
+                } else if( i == 4) {
+                    // Ustawienie layout_weight dla pozostałych komórek na 1
+                    layoutParams = new TableRow.LayoutParams(
+                            0,
+                            TableRow.LayoutParams.WRAP_CONTENT,
+                            0.7f);
+                }else {
+                    // Ustawienie layout_weight dla pozostałych komórek na 1
+                    layoutParams = new TableRow.LayoutParams(
+                            0,
+                            TableRow.LayoutParams.WRAP_CONTENT,
+                            1f);
+                }
+                headerTextView.setLayoutParams(layoutParams);
+
+                headerRow.addView(headerTextView);
+            }
+            // Dodawanie wiersza nagłówka do tabeli
+            dataTable.addView(headerRow);
+
+            // Tworzenie wierszy z danymi rekordów
+            do {
+                TableRow dataRow = new TableRow(this);
+                dataRow.setLayoutParams(new TableRow.LayoutParams(
+                        TableRow.LayoutParams.MATCH_PARENT,
+                        TableRow.LayoutParams.WRAP_CONTENT));
+
+                // Dodawanie komórek z danymi dla poszczególnych kolumn
+                for (int i = 0; i < columnNames.length; i++) {
+                    TextView dataTextView = new TextView(this);
+                    @SuppressLint("Range") String value = cursor.getString(cursor.getColumnIndex(columnNames[i]));
+                    dataTextView.setText(value);
+
+                    TableRow.LayoutParams layoutParams;
+                    if (i == 0) {
+                        // Ustawienie layout_weight dla komórki ID na 2
+                        layoutParams = new TableRow.LayoutParams(
+                                0,
+                                TableRow.LayoutParams.WRAP_CONTENT,
+                                0.3f);
+                    } else if( i == 1) {
+                        // Ustawienie layout_weight dla pozostałych komórek na 1
+                        layoutParams = new TableRow.LayoutParams(
+                                0,
+                                TableRow.LayoutParams.WRAP_CONTENT,
+                                0.8f);
+                    } else if( i == 3) {
+                        // Ustawienie layout_weight dla pozostałych komórek na 1
+                        layoutParams = new TableRow.LayoutParams(
+                                0,
+                                TableRow.LayoutParams.WRAP_CONTENT,
+                                0.4f);
+                    } else if( i == 4) {
+                        // Ustawienie layout_weight dla pozostałych komórek na 1
+                        layoutParams = new TableRow.LayoutParams(
+                                0,
+                                TableRow.LayoutParams.WRAP_CONTENT,
+                                0.7f);
+                    } else {
+                        // Ustawienie layout_weight dla pozostałych komórek na 1
+                        layoutParams = new TableRow.LayoutParams(
+                                0,
+                                TableRow.LayoutParams.WRAP_CONTENT,
+                                1f);
+                    }
+                    dataTextView.setLayoutParams(layoutParams);
+
+                    dataRow.addView(dataTextView);
+                }
+                // Dodawanie wiersza z danymi do tabeli
+                dataTable.addView(dataRow);
+            } while (cursor.moveToNext());
+        }
+        // Zamknij kursor po zakończeniu jego użytkowania
+        cursor.close();
+    }
+
+    public void fillRandomData(){
+        Random rand = new Random();
+
+        String [] randomNamesArr = { "Adam", "Michał", "Jakub", "Kacper", "Piotr", "Mateusz", "Szymon", "Dawid", "Tomasz", "Jan",
+                "Robert", "Marcin", "Krzysztof", "Andrzej", "Rafał", "Łukasz", "Patryk", "Grzegorz", "Bartosz", "Damian",
+                "Anna", "Katarzyna", "Marta", "Agnieszka", "Monika", "Joanna", "Magdalena", "Natalia", "Julia", "Karolina" };
+
+        String [] randomLastNamesArr = { "Nowak", "Kowalski", "Wiśniewski", "Wójcik", "Kowalczyk", "Kamiński", "Lewandowski", "Zieliński", "Szymański", "Woźniak",
+                "Dąbrowski", "Kozłowski", "Jankowski", "Mazur", "Kwiatkowski", "Krawczyk", "Piotrowski", "Grabowski", "Nowakowski", "Pawłowski",
+                "Adamczyk", "Dudek", "Wieczorek", "Jabłoński", "Król", "Majewski", "Olszewski", "Jaworski", "Wróbel", "Malinowski" };
+
+        int randomAge = rand.nextInt(100);
+
+        BigInteger randomPesel = peselGenerator();
+
+        int index = rand.nextInt(20);
+
+        if (nameEditTxt.getText().toString().isEmpty()) {
+            nameEditTxt.setText(randomNamesArr[index]);
+        }
+        if (lastNameEditTxt.getText().toString().isEmpty()) {
+            lastNameEditTxt.setText(randomLastNamesArr[index]);
+        }
+        if (ageEditTxt.getText().toString().isEmpty()) {
+            ageEditTxt.setText(String.valueOf(randomAge));
+        }
+        if (peselEditTxt.getText().toString().isEmpty()) {
+            peselEditTxt.setText(String.valueOf(randomPesel));
+        }
+
+        dm.insert(nameEditTxt.getText().toString(), lastNameEditTxt.getText().toString(), ageEditTxt.getText().toString(), genderSpinner.getSelectedItem().toString(), peselEditTxt.getText().toString());
+    }
+
+    public boolean checkBlanks (){
+        boolean result = false;
+
+        if (nameEditTxt.getText().toString().isEmpty()) {
+            result = true;
+        } else if (lastNameEditTxt.getText().toString().isEmpty()) {
+            result = true;
+        } else if (peselEditTxt.getText().toString().isEmpty()) {
+            result = true;
+        } else if (ageEditTxt.getText().toString().isEmpty()) {
+            result = true;
+        }
+
+        return result;
     }
 
     @Override
     public void onClick(View view) {
         switch(view.getId()){
             case R.id.insertBtn:{
-                dm.insert(nameEditTxt.getText().toString(), lastNameEditTxt.getText().toString());
+                if(checkBlanks()){
+                    fillRandomData();
+                } else{
+                    dm.insert(nameEditTxt.getText().toString(), lastNameEditTxt.getText().toString(), ageEditTxt.getText().toString(), genderSpinner.getSelectedItem().toString(), peselEditTxt.getText().toString());
+                }
                 break;
             }
             case R.id.selectBtn:{
+                dataTable.removeAllViews();
                 showData(dm.selectAll());
                 break;
             }
             case R.id.searchBtn:{
+                dataTable.removeAllViews();
                 showData(dm.searchName(searchEditTxt.getText().toString()));
                 break;
             }
             case R.id.deleteBtn:{
+                dataTable.removeAllViews();
                 dm.delete(deleteEditTxt.getText().toString());
+                showData(dm.selectAll());
                 break;
             }
         }
+    }
+
+    public static BigInteger peselGenerator() {
+        Random random = new Random();
+
+        // Pierwsza cyfra (rok urodzenia)
+        BigInteger pesel = BigInteger.valueOf(random.nextInt(9) + 1)
+                .multiply(BigInteger.TEN.pow(10));
+
+        // Druga i trzecia cyfra (miesiąc urodzenia)
+        int month = random.nextInt(12) + 1;
+        pesel = pesel.add(BigInteger.valueOf(month).multiply(BigInteger.TEN.pow(8)));
+
+        // Czwarta i piąta cyfra (dzień urodzenia)
+        int day = random.nextInt(31) + 1;
+        pesel = pesel.add(BigInteger.valueOf(day).multiply(BigInteger.TEN.pow(6)));
+
+        // Cyfry od szóstej do dziesiątej (losowe cyfry)
+        for (int i = 0; i < 5; i++) {
+            pesel = pesel.add(BigInteger.valueOf(random.nextInt(10)).multiply(BigInteger.TEN.pow(4 - i)));
+        }
+
+        // Ostatnia cyfra (cyfra kontrolna)
+        int checksum = calculatePeselChecksum(pesel);
+        pesel = pesel.add(BigInteger.valueOf(checksum));
+
+        return pesel;
+    }
+
+    private static int calculatePeselChecksum(BigInteger pesel) {
+        int[] weights = {1, 3, 7, 9, 1, 3, 7, 9, 1, 3};
+        int checksum = 0;
+
+        String peselString = pesel.toString();
+        for (int i = 0; i < 10; i++) {
+            int digit = Character.getNumericValue(peselString.charAt(i));
+            checksum += digit * weights[i];
+        }
+
+        int lastDigit = checksum % 10;
+        if (lastDigit != 0) {
+            lastDigit = 10 - lastDigit;
+        }
+        return lastDigit;
     }
 }
